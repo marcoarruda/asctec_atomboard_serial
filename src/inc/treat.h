fb0d69b780ac73c7d763200f2b80d026b0358d86
Marco <marco.nc.arruda@gmail.com>
2009-01-01 15:04:04 +0100

    conversao string to int

---
+++ src/inc/treat.h
@@ -0,0 +1,20 @@
+void treatConvert(std_msgs::String s, std_msgs::Int16MultiArray &array) {
+  // ignore brackets
+  std_msgs::String s1, sc;
+  std::stringstream ssc;
+  s1.data = s.data.substr(2, s.data.length() - 4);
+
+  int pos = 0, value;
+  array.data.clear();
+  while(pos > -1) {
+    pos = s1.data.find(';');
+    sc.data = s1.data.substr(0, pos);
+    s1.data = s1.data.substr(pos + 1, s1.data.length() - (pos + 1));
+    ssc.str(sc.data);
+    ssc >> value;
+    ssc.clear();
+    ssc.str("");
+    //ROS_INFO("%d", value);
+    array.data.push_back(value);
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 // String libs
 #include "std_msgs/String.h"
 #include "std_msgs/UInt16.h"
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/Int16MultiArray.h"
 #include <sstream>
+#include <iostream>
+#include <string>
 
 // Serial libs
 #include <unistd.h> // UNIX standard function definitions
@@ -13,6 +18,7 @@
 
 // Local libs
 #include "inc/serial.h"
+#include "inc/treat.h"
 
 int fd;
 
@@ -21,7 +27,7 @@ int main(int argc, char **argv)
   // ROS variables
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 10);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::Int16MultiArray>("chatter", 10);
   ros::Rate loop_rate(1);
   std_msgs::UInt16 count;
   count.data = 0;
@@ -35,17 +41,21 @@ int main(int argc, char **argv)
   while (ros::ok())
   {
     ss_toPublish.str(std::string());
-    ROS_INFO("loop [%d]", count.data);
+    //ROS_INFO("loop [%d]", count.data);
 
     // read serial
     readSerial(fd, ss_toPublish);
     toPublish.data = ss_toPublish.str();
-    ROS_INFO("%s", toPublish.data.c_str());
+    //ROS_INFO("%s", toPublish.data.c_str());
 
-    // validate and convert
+    // validate
+
+    // convert
+    std_msgs::Int16MultiArray array;
+    treatConvert(toPublish, array);
 
     // publish
-    chatter_pub.publish(toPublish);
+    chatter_pub.publish(array);
     ros::spinOnce();
     loop_rate.sleep();
     count.data++;
